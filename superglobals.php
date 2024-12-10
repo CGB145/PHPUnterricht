1f07ef5c8bb9233b3c19e22cebd990f5aaacc79c
CGB145 <b.caglan@outlook.com>
2024-12-10 13:31:31 +0100

    New File

---
+++ superglobals.php
@@ -0,0 +1,17 @@
+<?php $title = "Superglobale Arrays" ?>
+<!DOCTYPE html>
+<html lang="en">
+	
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> <?php echo $title ?>  </title>
+</head>
+	
+<body>
+<h1><?php echo $title?></h1>
+<h2>sind überall sichtbar, in globalen Scope (Gültigskeitbereich) und im lokalen Scope. <br>
+Sie werden vom System erzeugt und zur Verfügung gestellt.</h2>
+
+</body>
+</html>
